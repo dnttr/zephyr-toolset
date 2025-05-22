b96348b0773e4f9dd605db5bc59bdc97306cc076
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-22 20:14:08 +0200

    Implement getModifiers method to return modifiers based on component type

--- src/main/java/org/dnttr/zephyr/toolset/types/TypeMatch.java
+++ src/main/java/org/dnttr/zephyr/toolset/types/TypeMatch.java
@@ -9,6 +9,29 @@ import java.lang.reflect.Field;
 public class TypeMatch {
 
     public static int getModifiers(Field field) {
-        return 0;
+        final Class<?> parentType = field.getType();
+        final Class<?> componentType = parentType.getComponentType();
+
+        if (componentType == byte.class) {
+            return Type.BYTE.getModifiers();
+        } else if (componentType == boolean.class) {
+            return Type.BOOLEAN.getModifiers();
+        } else if (componentType == char.class) {
+            return Type.CHAR.getModifiers();
+        } else if (componentType == short.class) {
+            return Type.SHORT.getModifiers();
+        } else if (componentType == int.class) {
+            return Type.INT.getModifiers();
+        } else if (componentType == long.class) {
+            return Type.LONG.getModifiers();
+        } else if (componentType == float.class) {
+            return Type.FLOAT.getModifiers();
+        } else if (componentType == double.class) {
+            return Type.DOUBLE.getModifiers();
+        } else if (componentType == String.class) {
+            return Type.STRING.getModifiers();
+        } else {
+            throw new IllegalArgumentException("Unsupported type: " + componentType);
+        }
     }
 }
