1760a2a9cd755859baac0d7e91d1303f0b076848
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-22 20:07:25 +0200

    Add Type enum and TypeMatch class for data type representation and matching

---
+++ src/main/java/org/dnttr/zephyr/toolset/types/Type.java
@@ -0,0 +1,52 @@
+package org.dnttr.zephyr.toolset.types;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author dnttr
+ */
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public enum Type {
+
+    /*
+     * 8 bit
+     */
+
+    BYTE(0x1, 1),
+    BOOLEAN(0x2, 1),
+
+    /*
+     * 16 bit
+     */
+
+    SHORT(0x3, 2),
+    CHAR(0x4, 2),
+
+    /*
+     * 32 bit
+     */
+
+    INT(0x5, 4),
+    FLOAT(0x6, 4),
+
+    /*
+     * 64 bit
+     */
+
+    LONG(0x7, 8),
+    DOUBLE(0x8, 8),
+
+    /*
+     * Non-primitive types (therefore not fixed size)
+     */
+
+    STRING(0x9, 0);
+
+    private final int modifiers;
+    private int bytes;
+}
---
+++ src/main/java/org/dnttr/zephyr/toolset/types/TypeMatch.java
@@ -0,0 +1,14 @@
+package org.dnttr.zephyr.toolset.types;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author dnttr
+ */
+
+public class TypeMatch {
+
+    public static int getModifiers(Field field) {
+        return 0;
+    }
+}
